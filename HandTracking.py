a6b8148c2648cb68a6db674fe523c28c6bd41dfa
Mayank187 <mayank187@yahoo.com>
2021-06-09 15:19:54 +0530

    Added Comments

--- HandTracking.py
+++ HandTracking.py
@@ -2,23 +2,44 @@ import cv2
 import mediapipe as mp
 import time
 
+#Video capture from the main camera
 cap = cv2.VideoCapture(0)
+
+#IMporting hands from mediapipe.solutions file for hand detection
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+
+#To draw the landmarks on the image
 mpDraw = mp.solutions.drawing_utils
+
+
 p_time = 0
 c_time = 0
+
 while 1:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
+    #Getting Landmarks from result and drawint it on to the video feed
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #Getting pixel value
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 4 :
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    #FPS calculation
     c_time = time.time()
     fps = 1/(c_time-p_time)
     p_time = c_time
 
+    #Adding FPS Text to the Video
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
---
+++ volumeAdjustmentUsing Hand.py
@@ -0,0 +1,45 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as htm
+
+#Using pyautogui for increasing and decreasing volume of the system
+import pyautogui
+
+p_time = 0
+c_time = 0
+detector = htm.handDetector()
+
+# Video capture from the main camera
+cap = cv2.VideoCapture(0)
+
+p_thumb = 0
+c_thumb = 0
+while 1:
+    success, img = cap.read()
+
+    img = detector.findHands(img)
+
+    lmList = detector.findPosition(img)
+    if len(lmList) != 0:
+
+        p_thumb = p_thumb if p_thumb != 0 else lmList[4][1]
+        c_thumb = lmList[4][1]
+        #Using pervious thumb postion and current thumb position to reduce or increace volume
+        if p_thumb>c_thumb and p_thumb-c_thumb > 10:
+            pyautogui.press('volumeup')
+            p_thumb = c_thumb
+        elif p_thumb<c_thumb and c_thumb-p_thumb > 10:
+            pyautogui.press('volumedown')
+            p_thumb = c_thumb
+
+    # FPS calculation
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+
+    # Adding FPS Text to the Video
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
